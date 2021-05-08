8ffd0536f5f5d198e07ca8e9e33f34acd2363ac3
Aymen271 <Chabchoub@DESKTOP-54FI61U>
2021-05-08 06:03:39 +0100

    ded

--- Subscription/src/main/java/dari/tn/controller/SubscriptionController.java
+++ Subscription/src/main/java/dari/tn/controller/SubscriptionController.java
@@ -1,5 +1,11 @@
 package dari.tn.controller;
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 import org.ocpsoft.rewrite.annotation.Join;
 import org.ocpsoft.rewrite.el.ELBeanName;
@@ -29,37 +35,84 @@ import dari.tn.service.SubscriptionService;
 @Join(path = "/", to = "/login.jsf")public class SubscriptionController{
 	@Autowired  
 	SubscriptionService abonnementService;
+	public int subscription_id;
+	public String subscription_offer;
+	public String subscription_title;
+	public int subscription_price;
 	
-	@GetMapping("/getall")  
-	private List<Subscription> getAllSubs()   
+	public List<Subscription> ListSubs=new ArrayList<Subscription>();
+	
+	
+	public List<Subscription> getAllSubs()   
 	{  
 		 System.out.println("done");
-	return abonnementService.getAllSubscriptions();  
+	ListSubs= abonnementService.getAllSubscriptions();  
+	return ListSubs;  
 
 	}  
 	
+	public int getSubscription_id() {
+		return subscription_id;
+	}
+
+	public void setSubscription_id(int subscription_id) {
+		this.subscription_id = subscription_id;
+	}
+
+	public String getSubscription_offer() {
+		return subscription_offer;
+	}
+
+	public void setSubscription_offer(String subscription_offer) {
+		this.subscription_offer = subscription_offer;
+	}
+
+	public String getSubscription_title() {
+		return subscription_title;
+	}
+
+	public void setSubscription_title(String subscription_title) {
+		this.subscription_title = subscription_title;
+	}
+
+	public int getSubscription_price() {
+		return subscription_price;
+	}
+
+	public void setSubscription_price(int subscription_price) {
+		this.subscription_price = subscription_price;
+	}
+
+	public List<Subscription> getListSubs() {
+		return ListSubs;
+	}
+
+	public void setListSubs(List<Subscription> listSubs) {
+		ListSubs = listSubs;
+	}
+
 	@GetMapping("/get/{abonnement_id}")  
-	private Subscription getSubscritpion(@PathVariable("abonnement_id") int abonnement_id)   
+	public Subscription getSubscritpion(@PathVariable("abonnement_id") int abonnement_id)   
 	{  
 	return abonnementService.getSubscriptionById(abonnement_id);  
 	}  
 	
 	@DeleteMapping("/delete/{abonnement_id}")  
-	private void deleteSubscritpion(@PathVariable("abonnement_id") int abonnement_id)   
+	public void deleteSubscritpion(@PathVariable("abonnement_id") int abonnement_id)   
 	{  
 	abonnementService.delete(abonnement_id);  
 	}  
 	
 	@PostMapping("/subscribe")
 	@ResponseBody
-	private int saveSubscritpion(@RequestBody Subscription abonnement)   
+	public String saveSubscription(@RequestBody Subscription abonnement)   
 	{  
-	abonnementService.saveOrUpdate(abonnement);  
-	return abonnement.getSubscription_id();  
+	abonnementService.saveOrUpdate(abonnement);
+	return "/Subs.xhtml?faces-redirect=true";
 	}  
 	
 	@PutMapping("/update")  
-	private Subscription update(@RequestBody Subscription abonnement)   
+	public Subscription update(@RequestBody Subscription abonnement)   
 	{  
 	abonnementService.saveOrUpdate(abonnement);  
 	return abonnement;  
